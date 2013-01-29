82cea864f2df1c3252886cee2cb9c81e6b2518cb
Sjors Gielen <sjors@sjorsgielen.nl>
2013-01-30 00:17:11 +0100

    Upload documentation

---
+++ doc/navtree.js
@@ -0,0 +1,344 @@
+var NAVTREE =
+[
+  [ "DaZeus", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "ConfigReader", "class_config_reader.html", null ],
+      [ "ConfigReaderState", "struct_config_reader_state.html", null ],
+      [ "Database", "class_database.html", null ],
+      [ "DatabaseConfig", "struct_database_config.html", null ],
+      [ "DaZeus", "class_da_zeus.html", null ],
+      [ "dazeus_event_struct", "structdazeus__event__struct.html", null ],
+      [ "dazeus_scope_struct", "structdazeus__scope__struct.html", null ],
+      [ "dazeus_stringlist_struct", "structdazeus__stringlist__struct.html", null ],
+      [ "dazeus_struct", "structdazeus__struct.html", null ],
+      [ "ConfigReader::exception", "struct_config_reader_1_1exception.html", null ],
+      [ "GlobalConfig", "struct_global_config.html", null ],
+      [ "Network", "class_network.html", null ],
+      [ "NetworkConfig", "struct_network_config.html", null ],
+      [ "NetworkListener", "class_network_listener.html", null ],
+      [ "PluginComm", "class_plugin_comm.html", null ],
+      [ "PluginConfig", "struct_plugin_config.html", null ],
+      [ "Server", "class_server.html", null ],
+      [ "ServerConfig", "struct_server_config.html", null ],
+      [ "ServerSorter", "struct_server_sorter.html", null ],
+      [ "SocketConfig", "struct_socket_config.html", null ],
+      [ "TestListener", "class_test_listener.html", null ],
+      [ "utf8_encoding_error", "structutf8__encoding__error.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "ConfigReader", "class_config_reader.html", null ],
+      [ "ConfigReaderState", "struct_config_reader_state.html", null ],
+      [ "Database", "class_database.html", null ],
+      [ "DatabaseConfig", "struct_database_config.html", null ],
+      [ "DaZeus", "class_da_zeus.html", null ],
+      [ "dazeus_event_struct", "structdazeus__event__struct.html", null ],
+      [ "dazeus_scope_struct", "structdazeus__scope__struct.html", null ],
+      [ "dazeus_stringlist_struct", "structdazeus__stringlist__struct.html", null ],
+      [ "dazeus_struct", "structdazeus__struct.html", null ],
+      [ "ConfigReader::exception", "struct_config_reader_1_1exception.html", null ],
+      [ "GlobalConfig", "struct_global_config.html", null ],
+      [ "Network", "class_network.html", null ],
+      [ "NetworkConfig", "struct_network_config.html", null ],
+      [ "NetworkListener", "class_network_listener.html", [
+        [ "PluginComm", "class_plugin_comm.html", null ],
+        [ "TestListener", "class_test_listener.html", null ]
+      ] ],
+      [ "PluginConfig", "struct_plugin_config.html", null ],
+      [ "Server", "class_server.html", null ],
+      [ "ServerConfig", "struct_server_config.html", null ],
+      [ "ServerSorter", "struct_server_sorter.html", null ],
+      [ "SocketConfig", "struct_socket_config.html", null ],
+      [ "utf8_encoding_error", "structutf8__encoding__error.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "File List", "files.html", [
+      [ "bindings/libdazeus/include/libdazeus.h", "libdazeus_8h.html", null ],
+      [ "bindings/libdazeus/src/libdazeus.c", "libdazeus_8c.html", null ],
+      [ "contrib/libdazeus-irc/src/network.cpp", "network_8cpp.html", null ],
+      [ "contrib/libdazeus-irc/src/network.h", "network_8h.html", null ],
+      [ "contrib/libdazeus-irc/src/server.cpp", "server_8cpp.html", null ],
+      [ "contrib/libdazeus-irc/src/server.h", "server_8h.html", null ],
+      [ "contrib/libdazeus-irc/src/utils.cpp", "contrib_2libdazeus-irc_2src_2utils_8cpp.html", null ],
+      [ "contrib/libdazeus-irc/src/utils.h", "contrib_2libdazeus-irc_2src_2utils_8h.html", null ],
+      [ "contrib/libdazeus-irc/tests/compile.cpp", "compile_8cpp.html", null ],
+      [ "contrib/libdazeus-irc/tests/connect.cpp", "connect_8cpp.html", null ],
+      [ "contrib/libdazeus-irc/tests/connectevents.cpp", "connectevents_8cpp.html", null ],
+      [ "contrib/libdazeus-irc/tests/reconnect.cpp", "reconnect_8cpp.html", null ],
+      [ "src/config.cpp", "config_8cpp.html", null ],
+      [ "src/config.h", "config_8h.html", null ],
+      [ "src/database.cpp", "database_8cpp.html", null ],
+      [ "src/database.h", "database_8h.html", null ],
+      [ "src/dazeus.cpp", "dazeus_8cpp.html", null ],
+      [ "src/dazeus.h", "dazeus_8h.html", null ],
+      [ "src/dazeusglobal.h", "dazeusglobal_8h.html", null ],
+      [ "src/main.cpp", "main_8cpp.html", null ],
+      [ "src/plugincomm.cpp", "plugincomm_8cpp.html", null ],
+      [ "src/plugincomm.h", "plugincomm_8h.html", null ],
+      [ "src/utils.cpp", "src_2utils_8cpp.html", null ],
+      [ "src/utils.h", "src_2utils_8h.html", null ]
+    ] ],
+    [ "Directories", "dirs.html", [
+      [ "bindings", "dir_e9a1afb47a08e9bb64dad51bae0f2774.html", [
+        [ "libdazeus", "dir_40ca6315ddbcc6fcadb491a538df6a0b.html", [
+          [ "include", "dir_b0ce9f1ef90a533d160b814cc7d0f4ad.html", null ],
+          [ "src", "dir_fa9a7bb3024747ad52102b03f7a35cff.html", null ]
+        ] ]
+      ] ],
+      [ "contrib", "dir_f5d0918862111507f60aa64617660d64.html", [
+        [ "libdazeus-irc", "dir_963b9aa430ab1b5757b787e6d4ff9e7e.html", [
+          [ "src", "dir_0b6e360c4b805ba639b72ddafed65e8a.html", null ],
+          [ "tests", "dir_f1763e05f5a243da9cfa9c28b6d27661.html", null ]
+        ] ]
+      ] ],
+      [ "src", "dir_69729c1c964cfc521d652d708edafe9b.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
